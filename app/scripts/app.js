e5eae5c15377fe7e60475a3cfb8169ba16639721
baliza <baliza@gmail.com>
2015-12-17 22:20:31 +0000

    Week 2 Angular Forms and Form Validation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,7 +2,7 @@
 
 angular
   .module('confusionApp',[])
-     .controller('MenuController', ['$scope', function($scope) {
+      .controller('MenuController', ['$scope', function($scope) {
         
             $scope.showDetails = false;
                          
@@ -74,4 +74,28 @@ angular
               };
               
               
+        }])
+      .controller('ContactController', ['$scope', function($scope) {
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+       }])
+       .controller('FeedbackController', ['$scope', function($scope) {
+            $scope.sendFeedback = function() {
+                console.log($scope.feedback);
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"",
+                                       agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
         }]);
